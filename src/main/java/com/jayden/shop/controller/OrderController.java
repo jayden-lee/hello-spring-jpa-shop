582d3796b96800c95519c07d683e2fa431fdcec6
jayden-lee <gwanggi.lee@gmail.com>
2020-01-11 15:12:47 +0900

    상품 주문

---
+++ src/main/java/com/jayden/shop/controller/OrderController.java
@@ -0,0 +1,43 @@
+package com.jayden.shop.controller;
+
+import com.jayden.shop.domain.Member;
+import com.jayden.shop.domain.item.Item;
+import com.jayden.shop.service.ItemService;
+import com.jayden.shop.service.MemberService;
+import com.jayden.shop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
