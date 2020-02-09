9ccb751c2ac21209e6ad6e542158fd4df59b8ab3
jayden-lee <gwanggi.lee@gmail.com>
2020-02-10 01:15:23 +0900

    회원 조회 API

--- src/main/java/com/jayden/shop/api/MemberApiController.java
+++ src/main/java/com/jayden/shop/api/MemberApiController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,6 +35,16 @@ public class MemberApiController {
         memberService.update(id, request.getName());
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @GetMapping("/api/v1/members")
+    public Result getMembers() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+            .map(member -> new MemberDto(member.getName()))
+            .collect(toList());
+
+        return new Result(collect);
     }
 
     @Data
@@ -59,6 +72,18 @@ public class MemberApiController {
     @AllArgsConstructor
     static class UpdateMemberResponse {
         private Long id;
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
         private String name;
     }
 }
